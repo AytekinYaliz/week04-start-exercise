8371a0163be7a21c00081e078e45b8417e0dbe7d
Aytekin Yaliz <aytekin.yaliz@dentsuaegis.com>
2019-01-31 17:47:03 +0000

    single button for the visibility

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 
 console.log( 'js file loaded' );
 
-document.getElementById('mySpan1').addEventListener('click', function() {
-   document.getElementById('myDiv').style.visibility = "hidden"
+document.getElementById('mySpan').addEventListener('click', function() {
+   let visibility = document.getElementById('myDiv').style.visibility;
+
+   if(visibility === 'hidden') {
+      document.getElementById('myDiv').style.visibility = ""
+   } else {
+      document.getElementById('myDiv').style.visibility = "hidden"
+   }
 });
 
-document.getElementById('mySpan2').addEventListener('click', function() {
-   document.getElementById('myDiv').style.visibility = ""
-});
+// document.getElementById('mySpan1').addEventListener('click', function() {
+//    document.getElementById('myDiv').style.visibility = "hidden"
+// });
+//
+// document.getElementById('mySpan2').addEventListener('click', function() {
+//    document.getElementById('myDiv').style.visibility = ""
+// });
+
